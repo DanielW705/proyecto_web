ce12622b5b18276b14b8d8f3c84446fc71bd493e
Daniel <danielmtzlpz95@gmail.com>
2022-12-24 16:39:55 -0600

    Imagenes

---
+++ Js/site.js
@@ -0,0 +1,16 @@
+(() => {
+  let burge_box = document.querySelector(".left-menu .burger-box");
+
+  let close_menu = document.querySelector(".left-menu .btn-close-menu");
+
+  let menu = document.querySelector(".left-menu");
+
+  burge_box.addEventListener("click", () => {
+    // menu.classList.contains("close-menu")
+    menu.classList.replace("close-menu", "open-menu");
+  });
+
+  close_menu.addEventListener("click", () => {
+    menu.classList.replace("open-menu", "close-menu");
+  });
+})();
